8a1776e2541963d079e759434432e7768f9449e7
Cathy Fu <cathyfu1215@gmail.com>
2025-04-11 22:51:34 -0700

    fixed .env and database

--- Program.cs
+++ Program.cs
@@ -1,5 +1,10 @@
-using FeedbackApp.Services;
+using DotNetEnv;
 
+
+
+using FeedbackApp.Services;
+// This loads the environment variables from the .env file
+Env.Load();
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
